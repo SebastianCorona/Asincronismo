ba1c72e7bafeff237c0a90344e0b0ab08f483fcd
Sebastian Corona <sebastian.corona@optimen.com.mx>
2024-01-27 19:20:24 -0600

    Final

--- proyecto/src/assets/main.js
+++ proyecto/src/assets/main.js
@@ -1,11 +1,14 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UChOAQwIwOg3VktymoWSnh1Q&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '312fd96214msh74f717c4b333c05p15fe7ejsn6f12c1bb85e2',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-	}
-};
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+      'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+    }
+  };
+  
+
+const content = null || document.getElementById('content');
 
 async function fetchData(urlApi) {
     try {
@@ -20,9 +23,26 @@ async function fetchData(urlApi) {
 
 (async ()=> {
     try{
-        const viedeos = await fetchData(API);
-        
-    }catch{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `).slice(0,4).join('')}
 
+        `;
+        content.innerHTML = view;
+    }catch(error){
+        console.log(error)
     }
 })();
